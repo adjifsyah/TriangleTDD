e8c6002ac30d7eee8db37b0933dc4477f9a06715
adjifsyah <adji.firma>
2025-01-26 19:38:56 +0700

    commiitttt

--- TriangleTDD/ContentView.swift
+++ TriangleTDD/ContentView.swift
@@ -15,7 +15,7 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Hello, world!")
         }
-        // Perubahan test trigger
+        // Perubahan test trigger lgiiiiiii
         .padding()
     }
 }
